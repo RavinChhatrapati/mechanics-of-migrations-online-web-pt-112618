b17c79e8dddd4969d7954f521a50787aeb80ddb5
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-10 20:29:22 +0000

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,15 @@
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+def change #primary way for writing migrations
+create_table :artists do |t| # creates table named artists
+  t.string :name   # creates column name
+  t.string :genre
+  t.integer :age
+  t.string :hometown
+end
+end
+end
---
+++ db/mkdir/01_create_artists.rb
@@ -0,0 +1,15 @@
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+def change      # within change method. Primary method for writing migrations
+create_table :artists do |t| # create artist table
+t.string :name    # create column name inside artist table
+t.string :genre   # create column genre
+t.string :age     # create column age
+t.string :hometown # create column hometown
+end
+
+end
