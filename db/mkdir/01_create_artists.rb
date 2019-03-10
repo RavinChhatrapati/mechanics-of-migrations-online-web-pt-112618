cff8309b25da00b9b35682af46fb2392d16c41a5
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-10 18:52:00 +0000

    Automatically backed up by Learn

--- db/mkdir/01_create_artists.rb
+++ db/mkdir/01_create_artists.rb
@@ -4,4 +4,12 @@ class CreateArtists < ActiveRecord::Migration
  
   def down
   end
+def change      # within change method. Primary method for writing migrations 
+create_table :artists do |t| # create artist table
+t.string :name    # create column name inside artist table
+t.string :genre   # create column genre
+t.string :age     # create column age
+t.string :hometown # create column hometown 
+end
+
 end
