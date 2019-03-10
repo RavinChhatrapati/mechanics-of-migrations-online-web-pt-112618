bc115a545d473a18dc68ca96fee12fd8e9cd8bd0
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-10 20:15:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,7 +8,7 @@ def change #primary way for writing migrations
 create_table :artists do |t| # creates table named artists
   t.string :name   # creates column name
   t.string :genre
-  t.string :age
+  t.integer :age
   t.string :hometown
 end
 end
